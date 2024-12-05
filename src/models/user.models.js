0e05c4666abcdee1688e1866d029dd761a8ab0d6
Sumit <sumitprasadarya392@gmail.com>
2024-12-06 01:05:17 +0530

    Bcrypt and JWT for hashing

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -48,5 +50,11 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return;
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
